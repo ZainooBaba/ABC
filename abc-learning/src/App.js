12fdb0c821640071d412d4ec4891dfb88104652d
ZainSiddi <135157583+ZainSiddi@users.noreply.github.com>
2024-06-07 02:57:45 -0500

    Added upload functionality to csvReader and fixed bugs when reading csv

--- abc-learning/src/App.js
+++ abc-learning/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState} from "react";
+import {uploadData,removeStudentData,removeMentorData} from "./csvReader.ts"
 
 function App() {
   const [file, setFile] = useState()
@@ -7,18 +8,26 @@ function App() {
     setFile(event.target.files[0])
   }
 
-
   const uploadFile = () => {
-    console.log(file)
+      uploadData(file)
+  }
+
+    const removeData = () => {
+        removeStudentData()
+        removeMentorData()
     }
 
     return (
     <div className="App">
         <header className="App-header">
             <h1>ABC Learning</h1>
+            <br/>
+            <input type={"file"} onChange={handleChange}/>
+            <button onClick={uploadFile}>Upload</button>
+            <button onClick={removeData}>Remove Data</button>
     </header>
-           <input type={"file"} onChange={handleChange}/>
-      <button onClick={uploadFile}>Upload</button>
+
+
     </div>
   );
 }
--- abc-learning/src/csvReader.ts
+++ abc-learning/src/csvReader.ts
@@ -1,22 +1,15 @@
-// TODO 1: Read the csv file and return the data in the form of an array of objects
-// right now the function is returning a hard coded array of objects we need to read the csv file and return the data
-// in the format of a student or mentor array
-import { parse } from 'csv-parse';
-import * as fs from 'fs';
-import * as path from 'path';
-
+import {addToCollection, removeCollection } from './firebase.js';
 
 export type student = {
     firstName: string;
     lastName: string;
-    phoneNum: number;
+    phoneNum: string;
     nativeLang: string;
     grade: number;
     schoolName: string;
     schoolCity: string;
     inSEP: boolean;
     schoolState: string;
-    //TODO: Fill out the rest of properties ASAP
 };
 
 export type mentor = {
@@ -24,62 +17,98 @@ export type mentor = {
     firstName: string;
     lastName: string;
     isAdult: boolean;
-    phoneNum: number;
+    phoneNum: string;
     nativeLang: string;
     country: string;
     volunteerBuddy: string;
-
-    //TODO: Fill out the rest of properties ASAP
 };
 
-export function isStudentData(csvFile): boolean{
 
+function isStudentData(csvFile): boolean{
     return csvFile.name.includes("Student")
-    //TODO throw an error if the file is not a csv file
-    //TODO return true if thee csv file is for student data false if its mentor data.
-    //Use somethnig related to the file data not the file name
 }
 
-export async function getStudentData(csvFile): Promise <student []> {
-    //TODO throw an error if the file is not a csv file
-    //TODO return the student data in the form of an array of objects
+function readCSV(file: File | null): Promise<String[][]> {
+    return new Promise((resolve, reject) => {
+        if (file === null) {
+            reject("No file provided");
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const csvData = event.target.result as string;
+            const rows = csvData.split('\n');
+            const data = rows.map(row => row.split(',')); resolve(data);
+        };
+        reader.onerror = (error) => {
+            reject("Error reading the file: " + error);
+        };
+        reader.readAsText(file);
+    });
+}
+
+
+//
+
+async function getStudentData(csvFile): Promise <student []> {
     let data = await readCSV(csvFile) 
-    let students: student[] = []; 
-    for (let i = 1; i < data.length; i++) { 
-        let student: student = {firstName: data[i][0],lastName: data[i][1], phoneNum: parseInt(data[i][2]), nativeLang: (data[i][3]), grade: parseInt(data[i][4]), schoolName: data[i][5], schoolCity: data[i][6], inSEP: data[i][7], schoolState: data[i][8]} 
+    let students: student[] = [];
+    for (let i = 1; i < data.length; i++) {
+        let student: student = {
+            firstName: data[i][1].valueOf(),
+            lastName: data[i][2].valueOf(),
+            phoneNum: data[i][3].valueOf(),
+            nativeLang: data[i][4].valueOf(),
+            grade: parseInt(data[i][5].valueOf().charAt(0)),
+            schoolName: data[i][6].valueOf(),
+            schoolCity: data[i][7].valueOf(),
+            inSEP: (data[i][9].valueOf() == "Yes"),
+            schoolState: data[i][10].valueOf()
+        }
         students.push(student)
-        
-    } 
-    
-    return students; 
-
+    }
+    return students;
 }
 
-function readCSV(file: File | null): Promise<String[][]> { 
-    return new Promise((resolve, reject) => { 
-        if (file === null) { reject("No file provided"); return; } 
-        const reader = new FileReader(); 
-        reader.onload = (event) => { const csvData = event.target.result as string; 
-        const rows = csvData.split('\n'); 
-        const data = rows.map(row => row.split(',')); resolve(data);
-     }; 
-        reader.onerror = (error) => { 
-            reject("Error reading the file: " + error); }; reader.readAsText(file); }); }
-
-
-
-export async function getMentorData(csvFile): Promise <student []> {
-    //TODO throw an error if the file is not a csv file
-    //TODO return the student data in the form of an array of objects
-    let data = await readCSV(csvFile) 
-    let mentors: mentors[] = []; 
-    for (let i = 1; i < data.length; i++) { 
-        let mentor: mentor = {emailAddress: data[i][0], firstName: data[i][2], lastName: data[i][3], isAdult: (data[i][4]), phoneNum: data[i][5], nativeLang: data[i][5], country: data[i][6], volunteerBuddy: data[i][7]} 
+async function getMentorData(csvFile): Promise <mentor []> {
+    let data = await readCSV(csvFile)
+    let mentors: mentor[] = [];
+    for (let i = 1; i < data.length; i++) {
+        let mentor: mentor = {
+            emailAddress: data[i][1].valueOf(),
+            firstName: data[i][3].valueOf(),
+            lastName: data[i][4].valueOf(),
+            isAdult: (data[i][5].valueOf() == "Yes"),
+            phoneNum: data[i][6].valueOf(),
+            nativeLang: data[i][7].valueOf(),
+            country: data[i][8].valueOf(),
+            volunteerBuddy: data[i][11].valueOf()
+        }
         mentors.push(mentor)
-        
-    } 
-    
-    return mentors; 
+    }
+    return mentors;
+}
 
+export async function uploadData(data: File) {
+    if (isStudentData(data)) {
+        let students = await getStudentData(data);
+        students.forEach(async (student) => {
+            await addToCollection("Students", student);
+        });
+    }
+    else {
+        let mentors = await getMentorData(data);
+        mentors.forEach(async (mentor) => {
+            await addToCollection("Mentors", mentor);
+        });
+    }
+}
+
+export async function removeStudentData() {
+    await removeCollection("Students");
+}
+
+export async function removeMentorData() {
+    await removeCollection("Mentors");
 }
 
